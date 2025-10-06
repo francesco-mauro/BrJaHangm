557fed7ff81a3baf9071ddc5b1dbaf63cbc85e10
francesco-mauro <francesco.mauro180@gmail.com>
2025-10-06 14:58:42 +0200

    Hm setup

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String word = "pizza ";
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            wordState.add('_');
+
+        }
+
+
+
+        scanner.close();
+    }
+}
