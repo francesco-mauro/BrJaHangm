207072d323cd91ef8c8c641e2fd852b037fd343a
francesco-mauro <francesco.mauro180@gmail.com>
2025-10-07 17:58:48 +0200

    Working random words

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String word = "pizza";
+        //Hangman wordList
+        String filePath = "words.txt";
+        ArrayList<String> words = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                words.add(line.trim());
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file");
+            ;
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+            ;
+        }
+
+        Random random = new Random();
+
+        String word = words.get(random.nextInt(words.size()));
+
+        System.out.println(word);
 
         Scanner scanner = new Scanner(System.in);
 
